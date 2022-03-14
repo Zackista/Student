382b500fb6310338f26098fbd3adf4b17a4bb08f
Cristobal <colivarp@everis.com>
2022-03-14 01:10:44 -0300

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sucursalStudent';
+  
+  name = 'Cristóbal';
+  text1= "Event Binding";
+  textList:string[]  = [];
+  showList: boolean = false;
+  
+  studentList: any[] =[
+    {
+      name: "Cristóbal Olivares",
+      degree:"LCC"
+    },
+    {
+      name: "Anais Gonzalez",
+      degree:"TO"
+    },
+    {
+      name: "David Rodriguez",
+      degree:"Abogado"
+    },
+  ]
+
+  constructor(){
+
+  }
+
+  cambiarTexto(){
+    this.text1 ="cambio de nombre";
+  }
+
+  guardarTexto(text: string){
+    this.textList.push(text);
+  }
+
+  mostrarLista(){
+    this.showList = true;
+  }
+
+  getName(){
+    return setInterval(()=> 
+      this.name = "Belén", 3000
+    );
+  }
+
 }
